51b30dfce73e02198b761d01a669e244eab10060
Roadtrip Horror <aaronfrankenfield@gmail.com>
2020-12-10 19:14:42 -0500

    first commit in loops repo

---
+++ script.js
@@ -0,0 +1,36 @@
+// loops
+let moviesArray = ["It", "ET", "Up", "It 2", "D2", "Us", "Red", "Go"];
+
+// console.log(moviesArray[0]);
+// console.log(moviesArray[1]);
+// console.log(moviesArray[2]);
+
+// console.log(moviesArray.length);
+
+// let i = 0;
+
+// while (i < moviesArray.length) {
+//   console.log(moviesArray[i]);
+//   console.log(i);
+//   i++
+// }
+
+// let i = 1;
+
+// while(i < 20) {
+//   console.log("Hello World");
+//   console.log(i);
+//   i++ 
+// }
+
+// Lightning Exercise
+// Psychotic Bird
+// In your JavaScript file, write a while loop that outputs the phrase "TWEET TWEET" 100 times to the console.
+
+let i = 1;
+
+while (i <= 100) {
+  console.log("TWEET TWEET");
+  console.log(i)
+  i++
+}
